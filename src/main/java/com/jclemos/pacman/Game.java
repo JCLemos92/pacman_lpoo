611f998bb5bb1af627a944fb4c5d407b8a09028d
Joao Lemos <ee10201@fe.up.pt>
2020-05-31 02:06:31 +0100

    Added assyncrounous event handler

--- src/main/java/com/jclemos/pacman/Game.java
+++ src/main/java/com/jclemos/pacman/Game.java
@@ -2,11 +2,16 @@ package com.jclemos.pacman;
 
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.input.KeyStroke;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
 
 public class Game {
@@ -15,6 +20,7 @@ public class Game {
 
     private int x = 10;
     private int y = 10;
+
 
     public Game() throws IOException {
         terminal = new DefaultTerminalFactory().setInitialTerminalSize(new TerminalSize(59, 17)).createTerminal();
@@ -26,13 +32,39 @@ public class Game {
     }
 
     public void run() throws IOException {
-        draw();
+
+
+        KeyEventDispatcher keyEventDispatcher = new KeyEventDispatcher() {
+            @Override
+            public boolean dispatchKeyEvent(final KeyEvent e) {
+
+              System.out.println(e);
+              return true;
+            }
+        };
+
+        KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(keyEventDispatcher);
     }
         
     private void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(10, 10, new TextCharacter('X'));
+        screen.setCharacter(x, y, new TextCharacter('X'));
         screen.refresh();
     }
 
+    private void processKey(KeyStroke key){
+        System.out.println(key);
+        if (key.getKeyType() == KeyType.ArrowRight){
+            x += 1;
+        }
+        if (key.getKeyType() == KeyType.ArrowLeft){
+            x -= 1;
+        }
+        if (key.getKeyType() == KeyType.ArrowUp){
+            y -= 1;
+        }
+        if (key.getKeyType() == KeyType.ArrowDown){
+            y += 1;
+        }
+    }
 }
