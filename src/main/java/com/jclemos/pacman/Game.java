8cc082a6cee91ada2c3ee715b51b34c5dcc0dd84
Joao Lemos <ee10201@fe.up.pt>
2020-05-31 18:34:57 +0100

    Added findEmptySpots function

--- src/main/java/com/jclemos/pacman/Game.java
+++ src/main/java/com/jclemos/pacman/Game.java
@@ -1,7 +1,9 @@
 package com.jclemos.pacman;
 
+import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.graphics.TextGraphicsWriter;
 import com.googlecode.lanterna.input.KeyType;
@@ -28,6 +30,7 @@ public class Game {
     private Player pacman;
     private Wall wall;
     private Maze maze;
+    private NPC ghost;
     private int ghostNumber = 4;
 
 
@@ -38,7 +41,8 @@ public class Game {
         pacman = new Player(1, 1, DynamicEntity.Direction.Down, 1, 3);
         wall = new Wall(1, 1);
         maze = new Maze();
-        //ghost = new NPC(20, 14, DynamicEntity.Direction.Up, 2, 200);
+
+        ghost = new NPC(1, 1, DynamicEntity.Direction.Up, 2, 200);
         inputHandler = new InputHandler(pacman, maze);
 
         screen.setCursorPosition(null);   // we don't need a cursor
@@ -50,18 +54,25 @@ public class Game {
     }
 
     public void run() throws IOException, InterruptedException {
+        System.out.println(maze.generateEmptyPoint().getX());
+        System.out.println(maze.generateEmptyPoint().getY());
+
+
 
         while(true){
             sleep(60);
             draw();
         }
+
     }
         
     private void draw() throws IOException {
         screen.clear();
+        graphics.setBackgroundColor(TextColor.Factory.fromString("#ffe118"));
+        graphics.fillRectangle(new TerminalPosition(maze.generateEmptyPoint().getX(),maze.generateEmptyPoint().getY()), new TerminalSize(1, 1), ' ');
         maze.draw(graphics);
-        pacman.draw(graphics);
-        //ghost.draw(graphics);
+        //pacman.draw(graphics);
+        ghost.draw(graphics);
         screen.refresh();
     }
 }
